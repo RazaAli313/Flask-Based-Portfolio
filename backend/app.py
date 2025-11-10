eacf97b3607e7b09df14c801386e7108483162ce
Raza Ali <136004622+RazaAli313@users.noreply.github.com>
2025-11-10 12:16:44 +0500

    Set custom template and static folder for Flask app

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
 
 CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
